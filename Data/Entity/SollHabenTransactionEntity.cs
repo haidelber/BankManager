6fff273550c7bc2c82b902aefe758ae0972dfea5
Stefan Haider <shaider@sba-research.org>
2020-05-01 14:02:19 +0200

    SollHabenTransaction

---
+++ Data/Entity/SollHabenTransactionEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BankManager.Data.Entity
+{
+    public class SollHabenTransactionEntity : TransactionEntity
+    {
+        public decimal Soll { get; set; }
+        public decimal Haben { get; set; }
+
+        public override decimal Amount
+        {
+            get => Haben - Soll;
+            set
+            {
+                if (value >= 0)
+                {
+                    Haben = Math.Abs(value);
+                    Soll = 0;
+                }
+                else
+                {
+                    Haben = 0;
+                    Soll = Math.Abs(value);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{AvailabilityDate} {CurrencyIso} -{Soll} +{Haben} {Text}";
+        }
+    }
+}
---
+++ Data/Migrations/20190601075705_SollHaben.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BankManager.Data.Migrations
+{
+    public partial class SollHaben : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Haben",
+                table: "Transaction",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Soll",
+                table: "Transaction",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Haben",
+                table: "Transaction");
+
+            migrationBuilder.DropColumn(
+                name: "Soll",
+                table: "Transaction");
+        }
+    }
+}
